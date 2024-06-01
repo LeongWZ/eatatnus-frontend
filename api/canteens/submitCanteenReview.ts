8bdbb0088b009631462a5f1c9cfeaff4ffdbf26f
LeongWZ <leongweizhi03@gmail.com>
2024-06-01 17:22:20 +0800

    Changed backend api url

--- api/canteens/submitCanteenReview.ts
+++ api/canteens/submitCanteenReview.ts
@@ -1,8 +1,5 @@
 import { User } from "firebase/auth";
 
-// Change this to your local IP address;
-const localIp = "192.168.1.11:3000";
-
 type PostData = {
     canteenId: number,
     rating: number,
@@ -13,7 +10,7 @@ type PostData = {
 
 export default async function submitCanteenReview(user: User, data: PostData) {
     return user.getIdToken()
-        .then(token => fetch(`http://${localIp}/api/canteens/review`, {
+        .then(token => fetch(`https://eatatnus-backend.onrender.com/api/canteens/review`, {
             method: "POST",
             headers: {
                 "Accept": "application/json",
