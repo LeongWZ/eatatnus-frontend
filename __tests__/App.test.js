9b3158190a5d1a620c6834ce166de4685f1a975c
LeongWZ <leongweizhi03@gmail.com>
2024-07-05 20:54:22 +0800

    Init tests

---
+++ __tests__/App.test.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-undef */
+import React from "react";
+import App from "../app/(main)/index";
+import { render, screen } from "@testing-library/react-native";
+
+// It is recommended to use userEvent with fake timers
+// Some events involve duration so your tests may take a long time to run.
+jest.useFakeTimers();
+
+test("<App />", async () => {
+  render(<App />);
+  const tree = screen.toJSON();
+  expect(tree.children.length).toBe(2);
+});
